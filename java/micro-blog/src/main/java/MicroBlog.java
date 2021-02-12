a6c6d2fa2e3e31d55328047ae3ac71a7c689c677
Omari Sopromadze <coma.spurs@gmail.com>
2021-02-12 11:51:27 +0100

    add Microblog solution

---
+++ java/micro-blog/src/main/java/MicroBlog.java
@@ -0,0 +1,12 @@
+import java.nio.charset.StandardCharsets;
+
+class MicroBlog {
+    public String truncate(String input) {
+        return input.codePoints()
+                .limit(5)
+                .collect(StringBuilder::new,
+                        StringBuilder::appendCodePoint,
+                        StringBuilder::append)
+                .toString();
+    }
+}

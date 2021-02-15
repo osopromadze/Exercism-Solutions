7045a42c2880b200ef85eaaba0f914a3bd57e9c9
Omari Sopromadze <coma.spurs@gmail.com>
2021-02-16 00:41:00 +0100

    add Largest Series Product solution

---
+++ java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+class LargestSeriesProductCalculator {
+    private String inputNumber;
+
+    LargestSeriesProductCalculator(String inputNumber) {
+        boolean hasAnyLetter = inputNumber.chars().anyMatch(Character::isLetter);
+
+        if(hasAnyLetter) {
+            throw new IllegalArgumentException("String to search may only contain digits.");
+        }
+
+        this.inputNumber = inputNumber;
+    }
+
+    long calculateLargestProductForSeriesLength(int numberOfDigits) {
+        if(numberOfDigits < 0) {
+            throw new IllegalArgumentException("Series length must be non-negative.");
+        }
+
+        if(inputNumber.length() < numberOfDigits) {
+            throw new IllegalArgumentException("Series length must be less than or equal to the length of the string to search.");
+        }
+
+        long result = Long.MIN_VALUE;
+        String[] inputNumberArr = inputNumber.split("");
+        int index = 0;
+
+        while(index <= inputNumberArr.length - numberOfDigits ) {
+            long multiplicationResult = Arrays.stream(inputNumberArr)
+                    .skip(index)
+                    .limit(numberOfDigits)
+                    .mapToLong(Integer::new)
+                    .reduce(1, (total, element) -> total * element);
+
+            if(multiplicationResult > result) {
+                result = multiplicationResult;
+            }
+
+            index++;
+        }
+
+        return result;
+    }
+}

78739761765732a790a8a04dae13fe3b892d05b9
Omari Sopromadze <coma.spurs@gmail.com>
2021-03-22 17:55:30 +0400

    add java Error Handling solution

---
+++ java/error-handling/src/main/java/ErrorHandling.java
@@ -0,0 +1,57 @@
+import java.util.Optional;
+import java.util.stream.Stream;
+
+class ErrorHandling {
+
+    void handleErrorByThrowingIllegalArgumentException() {
+        throw new IllegalArgumentException();
+    }
+
+    void handleErrorByThrowingIllegalArgumentExceptionWithDetailMessage(String message) {
+        throw new IllegalArgumentException(message);
+    }
+
+    void handleErrorByThrowingAnyCheckedException() throws Exception {
+        throw new Exception();
+    }
+
+    void handleErrorByThrowingAnyCheckedExceptionWithDetailMessage(String message) throws Exception {
+        throw new Exception(message);
+    }
+
+    void handleErrorByThrowingAnyUncheckedException() {
+        throw new IllegalArgumentException();
+    }
+
+    void handleErrorByThrowingAnyUncheckedExceptionWithDetailMessage(String message) {
+        throw new IllegalArgumentException(message);
+    }
+
+    void handleErrorByThrowingCustomCheckedException() throws CustomCheckedException {
+        throw new CustomCheckedException();
+    }
+
+    void handleErrorByThrowingCustomCheckedExceptionWithDetailMessage(String message) throws CustomCheckedException {
+        throw new CustomCheckedException(message);
+    }
+
+    void handleErrorByThrowingCustomUncheckedException() {
+        throw new CustomUncheckedException();
+    }
+
+    void handleErrorByThrowingCustomUncheckedExceptionWithDetailMessage(String message) {
+        throw new CustomUncheckedException(message);
+    }
+
+    Optional<Integer> handleErrorByReturningOptionalInstance(String integer) {
+        Integer num = null;
+        try {
+            num = Integer.parseInt(integer);
+        } catch (NumberFormatException ignored) {
+
+        }
+
+        return Optional.ofNullable(num);
+    }
+
+}
